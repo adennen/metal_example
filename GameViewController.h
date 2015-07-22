ca379d64867feda11dfef89ba0f6d2bfe2132920
Aron Dennen <aron@rxwiki.com>
2015-07-21 21:14:21 -0400

    First

---
+++ GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  ipadajapF
+//
+//  Created by Aron Dennen on 6/25/15.
+//  Copyright (c) 2015 Aron Dennen. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GameViewController : UIViewController
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ipadajapF
+//
+//  Created by Aron Dennen on 6/25/15.
+//  Copyright (c) 2015 Aron Dennen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
